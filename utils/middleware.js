e379e3170229c8e56c7040a8ebc0bf7cd13e0c4a
mburuxx <njihiamburu.3@gmail.com>
2024-07-30 16:37:52 +0300

    Creating a new user

--- utils/middleware.js
+++
@@ -1,8 +0,0 @@
-// utils/middleware.js
-import express from 'express';
-
-const configureMiddleware = (app) => {
-  app.use(express.json()); // Add this line to parse JSON
-};
-
-export default configureMiddleware;
